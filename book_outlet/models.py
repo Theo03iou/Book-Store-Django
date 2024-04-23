4da0d9c1b43457dbd54ee87a147bde07a29cafc9
Theo Georgiou <75097082+Theo03iou@users.noreply.github.com>
2024-04-23 16:49:47 +0100

    Admin changes

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -10,14 +10,11 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestseller = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(default="", null=False, db_index=True, blank=True)
     
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
-    
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+
     
     def __str__(self):
         return f"{self.title} ({self.rating})"
