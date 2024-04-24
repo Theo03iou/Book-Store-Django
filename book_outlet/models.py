6a812b0e98ddffccfeb9860266a4f043e6ca7780
Theo Georgiou <75097082+Theo03iou@users.noreply.github.com>
2024-04-24 17:54:08 +0100

    Admin QOL changes

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -8,11 +8,17 @@ from django.utils.text import slugify
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+       return self.full_name()
 
 class Book(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50) 
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestseller = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, db_index=True, blank=True)
     
